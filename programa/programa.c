0967a7a18c813498312a83b8d67265f74552575e
German Vila <g.vilakrause@gmail.com>
2014-05-06 22:50:05 -0300

    Cambiado el protocolo a AF_INET

--- programa/programa.c
+++ programa/programa.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	struct addrinfo hints;
 	struct addrinfo *kernelInfo;
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
 	/*t_config *config=config_create("ubicacion?"); 	Para leer el arch de config
