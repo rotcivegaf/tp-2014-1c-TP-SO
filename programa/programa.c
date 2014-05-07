74f702446804e674ce1224aac54e668c93385900
German Vila <g.vilakrause@gmail.com>
2014-05-06 23:51:18 -0300

    Cambio en como se conecta al kernel (no es imp)

--- programa/programa.c
+++ programa/programa.c
@@ -13,33 +13,33 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <commons/config.h>
+#include <arpa/inet.h>
 
 #define IP "127.0.0.1" //Esto hay que cargarlo por archivo de config, que esta en la variable de entorno ANSISOP_CONFIG
-#define PUERTO "6667"
+#define PUERTO 6667
 
 
 int tamanioArchivo(FILE *archivo);
 
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints;
-	struct addrinfo *kernelInfo;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
+	struct sockaddr_in infoConexionKernel;
+
+	infoConexionKernel.sin_family = AF_INET;
+	infoConexionKernel.sin_addr.s_addr= inet_addr(IP);
+	infoConexionKernel.sin_port = htons(PUERTO);
+	memset(&infoConexionKernel.sin_zero, 0, sizeof infoConexionKernel.sin_zero[8]);
 
 	/*t_config *config=config_create("ubicacion?"); 	Para leer el arch de config
 	int ipKernel = config_get_int_value(config, "IP");
 	int puertoKernel = config_get_int_value(config, "Puerto");*/
 
-	getaddrinfo(IP, PUERTO, &hints, &kernelInfo); 	//Carga los datos de la conexion
-	int socketKernel = socket(kernelInfo->ai_family, kernelInfo->ai_socktype, kernelInfo->ai_protocol);
+	int socketKernel = socket(AF_INET,SOCK_STREAM,0);
 
-	if(connect(socketKernel, kernelInfo->ai_addr, kernelInfo->ai_addrlen)==-1){
+	if(connect(socketKernel,(struct sockaddr*)&infoConexionKernel, sizeof(struct sockaddr_in))==-1){
 		printf("Error:No se pudo conectar con el kernel \n");
 		exit(1);
 	}
-	freeaddrinfo(kernelInfo); 						//Ya no se necesita la info
 
 	FILE *scriptAProcesar = fopen(argv[1],"r");
 	int tamanioScript = tamanioArchivo(scriptAProcesar);
