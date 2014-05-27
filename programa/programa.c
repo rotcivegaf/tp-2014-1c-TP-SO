c11e08eb902a5c57d65f1fedf9a0eb35af93f1e8
German Vila <g.vilakrause@gmail.com>
2014-05-27 13:29:22 -0300

    Se conecta usando la biblioteca de func

--- programa/programa.c
+++ programa/programa.c
@@ -5,47 +5,27 @@
  *      Author: utnso
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
+#include "funciones.h"
 #include <commons/config.h>
-#include <arpa/inet.h>
 
 #define IP "127.0.0.1" //Esto hay que cargarlo por archivo de config, que esta en la variable de entorno ANSISOP_CONFIG
-#define PUERTO 6667
+#define PUERTO 5000
 
 
 int tamanioArchivo(FILE *archivo);
 
 int main(int argc, char *argv[])
 {
-	struct sockaddr_in infoConexionKernel;
-
-	infoConexionKernel.sin_family = AF_INET;
-	infoConexionKernel.sin_addr.s_addr= inet_addr(IP);
-	infoConexionKernel.sin_port = htons(PUERTO);
-	memset(&infoConexionKernel.sin_zero, 0, sizeof infoConexionKernel.sin_zero[8]);
-
-	/*t_config *config=config_create("ubicacion?"); 	Para leer el arch de config
-	int ipKernel = config_get_int_value(config, "IP");
-	int puertoKernel = config_get_int_value(config, "Puerto");*/
-
-	int socketKernel = socket(AF_INET,SOCK_STREAM,0);
-
-	if(connect(socketKernel,(struct sockaddr*)&infoConexionKernel, sizeof(struct sockaddr_in))==-1){
-		printf("Error:No se pudo conectar con el kernel \n");
-		exit(1);
-	}
+	/*t_config *config=config_create("configuracion.conf");
+	char *ipKernel=config_get_string_value(config, "IP");
+	int puertoKernel = config_get_int_value(config, "PUERTO");*/
 
 	FILE *scriptAProcesar = fopen(argv[1],"r");
 	int *tamanioScript = malloc(sizeof (int));
 	*tamanioScript=tamanioArchivo(scriptAProcesar);
 	char script[*tamanioScript];
 
+	int socket=conectarse(IP,PUERTO);
 
 	/*
 	//Envia el tamanio del script
@@ -57,11 +37,12 @@ int main(int argc, char *argv[])
 	while(!feof(scriptAProcesar))
 		script[i++]=fgetc(scriptAProcesar);
 
-	send(socketKernel, script, *tamanioScript, 0);
+	send(socket, &script, *tamanioScript, 0);
 
 
-	close(socketKernel);
+	close(socket);
 	fclose(scriptAProcesar);
+	free(tamanioScript);
 	return 0;
 }
 
