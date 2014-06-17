c0e9470c154d06c6e1c16efdf7b341e3833b97e3
vkger <g.vilakrause@gmail.com>
2014-06-16 21:01:30 -0300

    Agregado handshake y los demas tipos de msjs que recibe

--- programa/programa.c
+++ programa/programa.c
@@ -5,48 +5,85 @@
  *      Author: utnso
  */
 
-#include "funciones.h"
+#include "sockets_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include <commons/config.h>
+
 
 #define IP "127.0.0.1" //Esto hay que cargarlo por archivo de config, que esta en la variable de entorno ANSISOP_CONFIG
 #define PUERTO 5000
 
 
 int tamanioArchivo(FILE *archivo);
+void handshake_kernel(int soc_kernel);
 
 int main(int argc, char *argv[])
 {
 	/*t_config *config=config_create("configuracion.conf");
 	char *ipKernel=config_get_string_value(config, "IP");
-	int puertoKernel = config_get_int_value(config, "PUERTO");*/
+	char *puertoKernel = config_get_int_value(config, "PUERTO");*/
 
 	FILE *scriptAProcesar = fopen(argv[1],"r");
 	int tamanioScript=tamanioArchivo(scriptAProcesar);
 	char *script = malloc(tamanioScript);
 
-	int socket=conectarse(IP,PUERTO);
+	int socket_kernel=socket_crear_client(PUERTO,IP);
+	handshake_kernel(socket_kernel);
 
 	int i=0;
 	//Lee el script
 	while(!feof(scriptAProcesar))
 		script[i++]=fgetc(scriptAProcesar);
 
-	char *script_serializado = malloc(sizeof(int)+tamanioScript);
-	memcpy(script_serializado,&tamanioScript,sizeof(int));
-	memcpy(script_serializado+sizeof(int),script,tamanioScript);
+	t_mensaje *mensaje_cod_prog = crear_t_mensaje(CODIGO_SCRIPT,script,tamanioScript);
+	socket_send_serealizado(socket_kernel,mensaje_cod_prog);
 
-	send(socket,script_serializado,sizeof(int)+tamanioScript,0);
+	//espero la respuesta del kernel
+	int fin_ejecucion = 0;
+	t_mensaje *mensaje_recibido;
+	while (fin_ejecucion != 1){
+		mensaje_recibido = socket_recv_serealizado(socket_kernel);
 
-	int id_mensaje;
+		switch(mensaje_recibido->tipo){
+		case IMPRIMIR_VALOR:
+			int valor = atoi(mensaje_recibido->dato);
+			printf("%d",valor);
+			break;
+		case IMPRIMIR_TEXTO:
+			printf("%s",mensaje_recibido->dato);
+			break;
+		case CPU_DESCONEC:
+			printf("CPU DESCONECTADA\n");
+			fin_ejecucion = 1;
+			break;
+		case CONEC_CERRADA:
+			fin_ejecucion = 1;
+			break;
+		case FIN_EJECUCION:
+			printf("Ejecucion finalizada");
+			fin_ejecucion = 1;
+			break;
+		case MEM_INSUFICIENTE:
+			fin_ejecucion = 1;
+			printf("Memoria insuficiente\n");
+			//todo ERROR
+			break;
+		default:
+			sleep(1);
+			printf("%i\n",mensaje_recibido->tipo);
+			printf("El tipo de dato recibido es erroneo\n");
+			break;
+		}
+	}
 
-	recv(socket,&id_mensaje,sizeof(int),0);
-
-	if(id_mensaje==1)
-		printf("Acceso al sistema rechazado, no hay memoria suficiente");
-
-	close(socket);
+	close(socket_kernel);
 	fclose(scriptAProcesar);
 	free(script);
+	//config_destroy(config);
 	return 0;
 }
 
@@ -57,4 +94,12 @@ int tamanioArchivo(FILE *archivo){
 	return tamanio;
 }
 
+void handshake_kernel(int soc_kernel){
+	t_mensaje *handshake = crear_t_mensaje(HS_KERNEL_PROG,"",1);
+	socket_send_serealizado(soc_kernel,handshake);
 
+	handshake = socket_recv_serealizado(soc_kernel);
+
+	if(handshake->tipo == HS_KERNEL_PROG)
+		printf("KERNEL conectado\n");
+}
