f826b63ceff348d1d61dc0e8130042b1592c847d
rotcivegaf <victorfage@gmail.com>
2014-07-24 02:04:19 -0300

    cambio ruta del programa.ansisop

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/home/utnso/workspace/victor-tp-so/
 echo $LD_LIBRARY_PATH
 export ANSISOP_CONFIG=./programa/programa_config
 echo $ANSISOP_CONFIG
-sudo ln -s ./programa.ansisop /usr/bin/ansisop
+sudo ln -s /home/utnso/victor-tp-so/programa.ansisop /usr/bin/ansisop
 
 # Creo el proyecto ansisop
 sudo make allc
