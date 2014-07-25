26f93dd45da3ebbdaf183b438914b23c08dbace4
rotcivegaf <victorfage@gmail.com>
2014-07-24 21:39:51 -0300

    subo los scripts

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,21 @@ sudo ln -s /home/utnso/victor-tp-so/programa.ansisop /usr/bin/ansisop
 
 # Creo el proyecto ansisop
 sudo make allc
+
+# Hago ejecutables los scripts
+chmod +x ./scripts/completo.ansisop
+chmod +x ./scripts/consumidor.ansisop
+chmod +x ./scripts/deref-prioridad.ansisop
+chmod +x ./scripts/facil.ansisop
+chmod +x ./scripts/fibo.ansisop
+chmod +x ./scripts/for.ansisop
+chmod +x ./scripts/forES.ansisop
+chmod +x ./scripts/mcd.ansisop
+chmod +x ./scripts/pesado.ansisop
+chmod +x ./scripts/productor.ansisop
+chmod +x ./scripts/segfault.ansisop
+chmod +x ./scripts/stackoverflow.ansisop
+chmod +x ./scripts/vector.ansisop
+chmod +x ./scripts/bench/productor.ansisop
+chmod +x ./scripts/bench/starter.ansisop
+
