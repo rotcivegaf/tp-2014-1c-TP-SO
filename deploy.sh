b845c90fd90df3076099c7ad4e01a01070b9c0f2
Rotciv egaf <victorfage@gmail.com>
2014-07-29 03:10:44 -0300

    acomodo el .sh

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ cd ..
 cd ..
 
 # Declaro variables de entorno
-export LD_LIBRARY_PATH=/home/utnso/workspace/victor-tp-so/
+export LD_LIBRARY_PATH=/home/utnso/tp-2014-1c-hashtaggers/
 echo $LD_LIBRARY_PATH
 export ANSISOP_CONFIG=./programa/programa_config
 echo $ANSISOP_CONFIG
-sudo ln -s /home/utnso/victor-tp-so/programa.ansisop /usr/bin/ansisop
+sudo ln -s /home/utnso/tp-2014-1c-hashtaggers/programa.ansisop /usr/bin/ansisop
 
 # Creo el proyecto ansisop
 sudo make allc
